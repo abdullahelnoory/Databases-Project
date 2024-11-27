70c43611e054e7ed0310c8208807307e83671180
KarimZakzouk <kokyfarid82@yahoo.com>
2024-11-27 19:09:41 +0200

    Testing 2

--- user1/src/senddata.js
+++ user1/src/senddata.js
@@ -15,7 +15,10 @@ export default function datasend()
               const result = await response.json(); // Handle the server's response
               console.log('Response from server:', result);
             } catch (error) {
-              console.error('Error sending data:', error);
+              console.error('Error sending data: Type 121', error);
+              console.error('Error sending data: Type 121', error);
+              console.error('Error sending data: Type 121', error);
+
             }//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
             //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
             //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
