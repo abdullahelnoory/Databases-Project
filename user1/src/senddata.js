d4214328bfefc467fde75f84613b928bcbbe2778
Uderscore <162705423+Uderscore@users.noreply.github.com>
2024-11-27 21:06:59 +0200

    hi kimo

--- user1/src/senddata.js
+++ user1/src/senddata.js
@@ -19,6 +19,7 @@ export default function datasend()
             }//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
             //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
             //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+            //volpinjfedvoinogihgferoli
           };
 return(
 <div className="da">
