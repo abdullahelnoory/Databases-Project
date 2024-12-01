314cc84b9594983d7d6ad655829696afcff32520
KarimZakzouk <kokyfarid82@yahoo.com>
2024-12-01 16:28:07 +0200

    Route from Login to Users

--- src/Login.js
+++ src/Login.js
@@ -1,61 +1,75 @@
-import {useState} from 'react';
+import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
+export default function Login() {
+  const [forminput, setforminput] = useState({
+    email: "",
+    password: ""
+  })
+  const history = useHistory();
 
-export default function Login()
-{const [forminput , setforminput] = useState({email:"",
-    password:""})
+  const sendData = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch('http://localhost:6969/accounts/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(forminput),
+      });
 
-    const sendData = async () => {
-        
-      
-        try {
-          const response = await fetch('http://localhost:6969/accounts/login', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(forminput),
-          });
-      
-          const result = await response.json();
-          console.log('Response from server:', result);
-        } catch (error) {
-          console.error('Error sending data:', error);
-        }
-      };
+      const result = await response.json();
+      console.log(result.type);
 
-
-    return(
-        <div className='Login'>
-<form className="Login_box">
-   
-    <div className="input-container">
-    <label > Username</label>
-    <input value={forminput.email} type="text" onChange={(event) =>{
-      
-       setforminput({...forminput,email:event.target.value})
-       }
-    }>
-    </input>
-    </div>
-    <div className="input-container">
-    <label>Password</label>
-    <input value={forminput.password} type="password" onChange={(event) =>{
-      
-      setforminput({...forminput,password:event.target.value})
+      if (result.type === "Admin") {
+        history.push('/A');
+      } else if (result.type === "Manager"){
+        history.push('/M');
+      } else if (result.type === "Driver"){
+        history.push('/D');
+      } else if (result.type === "Passenger"){
+        history.push('/P');
       }
-   }></input>
-   </div>
 
-<button onClick={sendData} className="button">
-Login
-    </button>
-    <a  className=" alink" href="/Signup"> 
-        Sign up
-    </a>
+      console.log('Response from server:', result);
+    } catch (error) {
+      console.error('Error sending data:', error);
+    }
+  };
 
-    </form>
+
+  return (
+    <div className='Login'>
+      <form className="Login_box">
+
+        <div className="input-container">
+          <label > Username</label>
+          <input value={forminput.email} type="text" onChange={(event) => {
+
+            setforminput({ ...forminput, email: event.target.value })
+          }
+          }>
+          </input>
+        </div>
+        <div className="input-container">
+          <label>Password</label>
+          <input value={forminput.password} type="password" onChange={(event) => {
+
+            setforminput({ ...forminput, password: event.target.value })
+          }
+          }></input>
+        </div>
+
+        <button onClick={sendData} className="button">
+          Login
+        </button>
+        <a className=" alink" href="/Signup">
+          Sign up
+        </a>
+
+      </form>
     </div>
 
-    );
+  );
 }
